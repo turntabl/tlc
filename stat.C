d2ee2824aa1d1d10554f8f0a88e40920869c6d01
sam <sam@turntabl.io>
2019-10-19 11:58:29 +0000

    remove includes

--- stat.C
+++ stat.C
@@ -1,9 +1,4 @@
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/sysmacros.h>
 #include <iostream>
 #include <iomanip>
 
@@ -15,35 +10,33 @@ int main(int argc, char *argv[])
 
    if (argc != 2) {
        cerr <<  "Usage: " << argv[0] << " <pathname>" << endl;
-       exit(EXIT_FAILURE);
+       exit(1);
    }
 
    if (lstat(argv[1], &sb) == -1) {
        perror("lstat");
-       exit(EXIT_FAILURE);
+       exit(1);
    }
 
-   cout << "ID of containing device: "<< major(sb.st_dev) << "," << minor(sb.st_dev) << endl;
-
-   cout << "File type: " << endl;
+   cout << "File type: " ;
 
    switch (sb.st_mode & S_IFMT) {
 	case S_IFDIR:  
-		cout << "directory" << endl;
+		cout << "Directory" << endl;
 	   	break;
 	case S_IFLNK:  
-	   	cout << "symbolic link" << endl;
+	   	cout << "Symbolic link" << endl;
 	   	break;
    	case S_IFREG: 
-		cout << "regular file" << endl;       
+		cout << "Regular file" << endl;       
 		break;
      	default:      
-	       cout << "something else" << endl;
+	       cout << "Something else" << endl;
    }
    
-   cout << setw(13) << "inodenumber: " << sb.st_ino << endl; 
-   cout << setw(13) << "Mode: " << sb.st_mode << endl;
-   cout << setw(13) << "Owner: " << sb.st_uid << endl;
+   cout << setw(14) << "inode number: " << sb.st_ino << endl; 
+   cout << setw(14) << "Mode: " << sb.st_mode << endl;
+   cout << setw(14) << "Owner: " << sb.st_uid << endl;
 
-   exit(EXIT_SUCCESS);
+   exit(0);
 }
